499e30581c7d76ea3baf9aa49ab4722f8ccc8d2e
Mateuswuz <116360372+Mateuswuz@users.noreply.github.com>
2022-11-28 15:25:45 -0300

    Add files via upload

---
+++ assets/preload/custom_notetypes/Backers.lua
@@ -0,0 +1,14 @@
+function onCreate()
+	for i = 0, getProperty('unspawnNotes.length')-1 do
+		if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'Backers' then --Check if the note on the chart is a Bullet Note
+			setPropertyFromGroup('unspawnNotes', i, 'noAnimation', true); -- make it so original character doesn't sing these notes
+		end
+	end
+end
+
+function opponentNoteHit(id, noteData, noteType, isSustainNote)
+if noteType == 'Backers' then
+triggerEvent('Screen Shake','0.1','0.002')
+triggerEvent('Add Camera Zoom','0.015','0.015')
+end
+end
